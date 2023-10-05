9f3ba38f1f288fa2aaa0908380c49a29e453e714
RizqalAzizhan <brisoes231205@gmail.com>
2023-10-05 10:57:03 +0700

    delete

--- register.php
+++ register.php
@@ -1,20 +1,4 @@
-<?php 
-require 'functions.php';
 
-if( isset($_POST["register"]) ) {
-
-	if( registrasi($_POST) > 0 ) {
-		echo "<script>
-				alert('New user successfully added!');
-				document.location.href = 'login.php';
-			  </script>";
-	} else {
-		echo mysqli_error($conn);
-	}
-
-}
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
